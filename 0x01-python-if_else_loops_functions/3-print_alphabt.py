bfbbbc50dc6fe3a15aeef7f64ad99af065802651
vick-ram <vickramodero6@gmail.com>
2024-01-02 09:52:46 -0800

    3. When I was having that alphabet soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-print("{}".format("".join(chr(i) for i in range(97, 123)
-    if chr(i) not in 'qe')), end='')
+print("{}".format("".join(chr(i) for i in range(97, 123)\
+if chr(i) not in 'qe')), end='')
