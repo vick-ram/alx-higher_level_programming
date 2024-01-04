40fc191783af1ea7db4b779066f7c4acb18b06eb
vick-ram <vickramodero6@gmail.com>
2024-01-04 06:47:55 -0800

    8. ByteCode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from magic_calculation_102 import add, sub
-    
-    def margic_calculation(a, b):
-        if a < b:
-            c = add(a, b)
+from magic_calculation_102 import add, sub
+
+def margic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
         for i in range(4, 6):
-                c = add(c, i)
-            return c
-        else:
-            return sub(a, b)
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
