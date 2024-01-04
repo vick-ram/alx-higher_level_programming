01612618e3f53fe4c7d4f812b67b75e1be1809a4
vick-ram <vickramodero6@gmail.com>
2024-01-04 07:23:30 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,16 @@
-#!/usr/bin/python3
-
-def margic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+def magic_calculation(a, b):
+    # Import the add and sub functions from the magic_calculation_102 module
+    from magic_calculation_102 import add, sub
+    # Compare a and b
     if a < b:
+        # If a is less than b, assign the result of add(a, b) to c
         c = add(a, b)
+        # Loop from 4 to 6 (excluding 6)
         for i in range(4, 6):
+            # Add i to c
             c = add(c, i)
+        # Return c
         return c
     else:
+        # If a is not less than b, return the result of sub(a, b)
         return sub(a, b)
