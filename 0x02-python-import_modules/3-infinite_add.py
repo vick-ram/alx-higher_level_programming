8fe80543ca3f734376706977179d08001b71a2f2
vick-ram <vickramodero6@gmail.com>
2024-01-04 05:19:27 -0800

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-def infinite_add(args):
-    result = sum(int(arg) for arg in args)
-    print(result)
+    def infinite_add(args):
+        result = sum(int(arg) for arg in args)
+        print(result)
 
 if __name__ == "__main__":
     infinite_add(argv[1:])
