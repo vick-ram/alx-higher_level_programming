e789dbac8ef7f7a23bfb350c22af6d24e89d0cdc
vick-ram <vickramodero6@gmail.com>
2024-01-04 04:44:02 -0800

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+def infinite_add(args):
+    result = sum(int(arg) for arg in args)
+    print(result)
+
+if __name__ == "__main__":
+    infinite_add(argv[1:])
