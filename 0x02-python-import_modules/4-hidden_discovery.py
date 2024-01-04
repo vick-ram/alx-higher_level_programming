b45032f74d48ea0c0cb319186a8e814e3a4e6aec
vick-ram <vickramodero6@gmail.com>
2024-01-04 05:25:04 -0800

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    
+    import hidden_4    
     names = dir(hidden_4)
     for name in sorted(names):
         if not name.startswith('__'):
