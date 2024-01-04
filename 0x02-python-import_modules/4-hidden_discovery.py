38a185f40be889f2069a489c6642e55c68fd3a77
vick-ram <vickramodero6@gmail.com>
2024-01-04 05:44:39 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4    
     names = dir(hidden_4)
     for name in sorted(names):
-        if not name.startswith('__'):
+        if not name.startswith("__"):
             print("{}".format(name))
