949a10e579d21f6035e868b93548af7eb4007ea1
vick-ram <vickramodero6@gmail.com>
2024-01-05 20:50:53 -0800

    9. Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    def max_integer(my_list=[]):
+        if len(my_list) == 0
+        for i in my_list:
+            if i > len(my_list[0:-1]):
+                return i
