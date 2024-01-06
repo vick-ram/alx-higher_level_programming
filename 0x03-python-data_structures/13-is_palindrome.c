f9094bf23025de54ed7f3ac94b64c2a4e9910f36
vick-ram <vickramodero6@gmail.com>
2024-01-06 10:25:08 -0800

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,46 @@
 #include "lists.h"
 /**
+*compare_and_reverse - a helper function to reverse
+second half of a string and compare it with first
+*@head: - first struct element
+*@mid: - middle element
+*Return: - returns reversed
+*/
+int compare_and_reverse(listint_t **head, listint_t *mid)
+{
+	listint_t *prev = NULL, *curr = mid, *next;
+	int res = 1;
+	listint_t *p1, *p2;
+
+	while (curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	p1 = *head, p2 = prev;
+	while (p1 != NULL && p2 != NULL)
+	{
+		if (p1->n != p2->n)
+		{
+			res = 0;
+			break;
+		}
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+	curr = prev;
+	while (curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	return (res);
+}
+/**
 *is_palindrome - checks if linked list is palindrome
 *@head: first list element
 *Return: - returns 0 if not and 1 if is
@@ -7,29 +48,15 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *slow = *head, *fast = *head;
-	listint_t *prev = NULL, *temp, *mid = NULL;
-	int is_palindrome = 1;
+	listint_t *mid = NULL;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 	while (fast != NULL && fast->next != NULL)
 	{
 		fast = fast->next->next;
-		temp = slow->next;
-		slow->next = prev;
-		prev = slow;
-		slow = temp;
+		slow = slow->next;
 	}
-	mid = (fast == NULL) ? prev : slow;
-	while (prev != NULL && mid != NULL)
-	{
-		if (prev->n != mid->n)
-		{
-			is_palindrome = 0;
-			break;
-		}
-		prev = prev->next;
-		mid = mid->next;
-	}
-	return (is_palindrome);
+	mid = (fast == NULL) ? slow : slow->next;
+	return (compare_and_reverse(head, mid));
 }
