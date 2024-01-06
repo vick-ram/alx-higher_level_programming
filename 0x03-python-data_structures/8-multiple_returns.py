c7332ff53955413276a94f2a2e735b38cdc9576b
vick-ram <vickramodero6@gmail.com>
2024-01-05 21:39:00 -0800

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def multiple_returns(sentence):
-        if not sentence:
-            return (0, None)
-        return (len(sentence), sentence[0])
+def multiple_returns(sentence):
+    if not sentence:
+        return (0, None)
+    return (len(sentence), sentence[0])
