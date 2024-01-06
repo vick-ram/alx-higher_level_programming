b9c14a7e97e6e8b196c808463fee5287fee6dd24
vick-ram <vickramodero6@gmail.com>
2024-01-05 21:30:07 -0800

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def no_c(my_string):
-        new_string = []
-        for char in my_string:
-            if char not in "cC":
-                new_string.append(char)
-        return "".join(new_string)
+def no_c(my_string):
+    new_string = []
+    for char in my_string:
+        if char not in "cC":
+            new_string.append(char)
+    return "".join(new_string)
