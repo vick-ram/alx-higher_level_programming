36c750e5d37ecd12e05b0395a2254bb8602f4d00
vick-ram <vickramodero6@gmail.com>
2024-01-08 08:31:30 -0800

    14. CPython #0: Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,29 @@
+#include <Python.h>
+/**
+*print_python_list_info - function to print python basic info
+*@p: - python object param
+*Return: - returns nothing
+*/
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, allocated, i;
+	PyObject *item;
+
+	if (!PyList_Check(p))
+	{
+		PyErr_SerString(PyExc_TypeError, "Input must be a list.");
+		return;
+	}
+
+	size = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
