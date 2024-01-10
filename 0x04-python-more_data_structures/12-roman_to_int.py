4f17355eeeb5c38fe93ad39fbafeea8a28f59726
vick-ram <vickramodero6@gmail.com>
2024-01-10 05:46:18 -0800

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,10 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+        }
     total = 0
     prev_value = 0
     for char in reversed(roman_string):
