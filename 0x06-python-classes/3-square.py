baacc8637262b6cc912e142816feced2498606c6
vick-ram <vickramodero6@gmail.com>
2024-01-23 07:17:20 -0800

    3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
 This is the 3-square module.
-
-This module defines a Square class that represents a square. The Square class includes private instance attributes for size, an __init__ method to initialize the square, an area method to calculate the area of the square, and a my_print method to print the square.
+This module defines a Square class that represents a
+square. The Square class includes private instance
+attributes for size, an __init__ method to initialize
+the square, an area method to calculate the area of the
+square, and a my_print method to print the square.
 """
+
 
 class Square:
     """
-    This class represents a square and defines its properties and behavior.
-
+    This class represents a square and
+    defines its properties and behavior.
     Attributes:
         size (int): The size of the square.
-
     Methods:
-        __init__(self, size=0): Initializes a new Square instance with an optional size.
+        __init__(self, size=0): Initializes a new
+        Square instance with an optional size.
         area(self): Returns the area of the square.
     """
 
@@ -23,7 +27,6 @@ class Square:
 
         Args:
             size (int): The size of the square (default is 0).
-
         Raises:
             TypeError: If the size is not an integer.
             ValueError: If the size is less than 0.
@@ -38,7 +41,6 @@ class Square:
     def area(self):
         """
         Returns the area of the square.
-
         Returns:
             int: The area of the square.
         """
