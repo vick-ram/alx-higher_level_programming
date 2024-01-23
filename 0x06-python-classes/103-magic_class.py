6dae7a53cf75dcdd0efd333aee2d8213f69da667
vick-ram <vickramodero6@gmail.com>
2024-01-23 08:17:47 -0800

    10. ByteCode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,20 +2,24 @@
 """This is 103-magic_class module"""
 import math
 
+
 class MagicClass:
     """
     This is the MagicClass.
-
-    This class defines a circle by its radius. The radius is a private instance attribute. The class provides a getter and setter for the radius attribute to ensure that it is a number (float or integer) and is not less than 0. The class also provides methods to calculate the area and circumference of the circle.
+    This class defines a circle by its radius.
+    The radius is a private instance attribute.
+    The class provides a getter and setter for the radius
+    attribute to ensure that it is a number (float or integer)
+    and is not less than 0. The class also provides
+    methods to calculate the area and circumference of the circle.
     """
 
     def __init__(self, radius=0):
         """
         Initializes an instance of the MagicClass.
-
         Parameters:
-        radius (number, optional): The radius of the circle. Defaults to 0. Radius must be a number (float or integer) and must be >= 0.
-
+        radius (number, optional): The radius of the circle. Defaults
+        to 0. Radius must be a number (float or integer) and must be >= 0.
         Raises:
         TypeError: If the radius is not a number (float or integer).
         """
@@ -27,7 +31,6 @@ class MagicClass:
     def area(self):
         """
         Calculates the area of the circle.
-
         Returns:
         float: The area of the circle.
         """
@@ -36,9 +39,7 @@ class MagicClass:
     def circumference(self):
         """
         Calculates the circumference of the circle.
-
         Returns:
         float: The circumference of the circle.
         """
         return 2 * math.pi * self.__radius
-
