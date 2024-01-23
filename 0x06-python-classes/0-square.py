236ba45b296c08f9befcbebc99cbe0af5d41dfdb
vick-ram <vickramodero6@gmail.com>
2024-01-23 04:27:27 -0800

    0. My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This is Square class
+
+    This class does nothing and performs no operations
+    The __init__ method is used to initialize the instances of this class.
+    """
+    def __init__(self):
+        """
+	Initializes an instance of Square.
+
+	Currently, this method does not initialize any instance variables.
+	"""
+        pass
