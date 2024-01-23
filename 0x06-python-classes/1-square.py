60026c0638d2e2fa8dd1e955da802ed6e0fac951
vick-ram <vickramodero6@gmail.com>
2024-01-23 07:10:51 -0800

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""This module only contains a single class that defines a private instance variabble of a class.
+"""This module only contains a single class that defines
+a private instance variabble of a class.
 """
 
 
@@ -8,10 +9,10 @@ class Square:
     """This class Square only defines a private instance variable."""
 
     def __init__(self, size):
-        """This init method, the __init__ method fires first when the class is instantiated
+        """This init method, the __init__ method fires first when
+        the class is instantiated
 
         Args:
             size (int): size param
- 
           """
         self.__size = size
