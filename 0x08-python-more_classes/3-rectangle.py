577df3d8d5526cdacec194a35541ea42e717650a
vick-ram <vickramodero6@gmail.com>
2024-01-29 07:59:54 -0800

    3. String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -82,6 +82,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
     def __str__(self):
         """returns a well formatted string"""
         if self.__width == 0 or self.__height == 0:
