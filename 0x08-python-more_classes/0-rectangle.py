f071f88e4f20493bdb5b0cc1181a0787b0bef47c
vick-ram <vickramodero6@gmail.com>
2024-01-29 09:38:00 -0800

    0. Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Module 0-rectangle that only defines a class"""
 
 
 class Rectangle:
+    """Rectangle class that does nothing"""
     def __init__(self):
+        """This init method initializes no class object"""
         pass
