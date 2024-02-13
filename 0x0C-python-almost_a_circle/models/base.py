43c5d3e47ba1fba258c60bc3dfd1563467882dc9
vickram <vickramodero6@gmail.com>
2024-02-13 11:05:00 +0300

    updated the Base class by defining staticmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """base module"""
+import json
 
 
 class Base:
@@ -13,3 +14,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns JSON string representation
+        list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            json.dumps(list_dictionaries)
