23e0e0aeb73807a3e41d023ced8eb88de9338755
vickram <vickramodero6@gmail.com>
2024-02-13 11:38:19 +0300

    Added some static methods and classmethods into Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,41 @@ class Base:
             return "[]"
         else:
             json.dumps(list_dictionaries)
+
+    @classmethod
+    def from_json_string(cls, json_string):
+        """Returns the list of the JSON string representation"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            new_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            new_instance = cls(1)
+        new_instance.update(**dictionary)
+        return new_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as file:
+                return ([cls.create(**obj)
+                        for obj in cls.from_json_string(file.read())])
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as file:
+            json_string = cls.to_json_string(
+                [obj.to_dictionary() for obj in list_objs]
+            )
+            file.write(json_string)
