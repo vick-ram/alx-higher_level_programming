83d5da595af93d6e1b320d885516eac4274f915e
vick-ram <vickramodero6@gmail.com>
2024-03-12 19:55:26 -0700

    full table description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- Prints the full description of the first_table from database hbtn_0c_0
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE hbtn_0c_0.first_table;
