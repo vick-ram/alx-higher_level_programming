63cf85473aba8f9a4a24f7a6f815bdd9984730d6
vick-ram <vickramodero6@gmail.com>
2024-03-13 14:48:12 -0700

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,8 +3,4 @@ SELECT cities.id, cities.name, (
 	SELECT states.name
 	FROM states
 	WHERE cities.state_id = states.id
-)
-AS state_name
-FROM cities
-ORDER BY cities.id
-ASC;
+) AS state_name FROM cities ORDER BY cities.id ASC;
