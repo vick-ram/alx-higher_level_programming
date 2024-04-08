392b12ac95b201b49c48936c44db0927e26e05cf
vickram <vickramodero6@gmail.com>
2024-04-09 01:04:45 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0) {
+if (process.argv.length === 2) {
 	console.log('No argument');
-} else if (args.length === 1) {
+} else if (process.argv.length === 3) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
