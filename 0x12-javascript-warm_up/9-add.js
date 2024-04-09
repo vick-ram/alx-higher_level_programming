ecf8659768ce1c939f5a6aabed0ec8cd036aa0d1
vick-ram <vickramodero6@gmail.com>
2024-04-09 00:12:24 -0700

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-function add(a, b) {
-	const sum = a + b;
-	console.log(sum);
+function add (a, b) {
+  const sum = a + b;
+  console.log(sum);
 }
 
 add(Number(process.argv[2]), Number(process.argv[3]));
