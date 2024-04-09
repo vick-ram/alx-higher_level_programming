f03ce9942cda46773f80e83ad9ead07ea53ead97
vick-ram <vickramodero6@gmail.com>
2024-04-09 00:13:47 -0700

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-function factorial(n) {
-	if (n <= 1) {
-		return 1;
-	} else if (isNaN(n)) {
-		return 1;
-	}
-	const fact = n *factorial(n -1);
-	return fact;
+function factorial (n) {
+  if (n <= 1) {
+    return 1;
+  } else if (isNaN(n)) {
+    return 1;
+  }
+  const fact = n * factorial(n - 1);
+  return fact;
 }
 
 const result = factorial(Number(process.argv[2]));
