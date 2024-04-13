a7a5fbd0b9e02c04579846d6350f9a1ff7190015
vick-ram <vickramodero6@gmail.com>
2024-04-13 08:59:34 -0700

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,15 +10,11 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-
     db = db_connector.connect_to_db(username, password, database)
     cur = db.cursor()
-
     cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC""")
     results = cur.fetchall()
-
     for row in results:
         print(row)
-
     cur.close()
     db.close()
