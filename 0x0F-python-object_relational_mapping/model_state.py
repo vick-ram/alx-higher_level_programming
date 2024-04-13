b6dbedeb0682d7a89412e5ca058efd9df1143ba2
vick-ram <vickramodero6@gmail.com>
2024-04-13 12:10:21 -0700

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,8 +5,12 @@ import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from base import Base
 
+
 class State(Base):
     """Class representing the states table"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(
+        Integer, primary_key=True,
+        nullable=False, autoincrement=True
+    )
     name = Column(String(128), nullable=False)
