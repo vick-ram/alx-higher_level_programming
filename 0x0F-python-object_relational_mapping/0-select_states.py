3fa19aa124e9bc01c28896f4aaa1de3736d2f099
vick-ram <vickramodero6@gmail.com>
2024-04-13 13:26:00 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,17 +4,16 @@ import sys
 import db_connector
 
 if __name__ == "__main__":
-    """
-    The main function of the script.
-    It takes command line arguments for the username, password,
-    and database name,
-    connects to the database, fetches all states, and prints them.
-    """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = db_connector.connect_to_db(username, password, database)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        database=database
+    )
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
