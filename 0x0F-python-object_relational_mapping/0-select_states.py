ca8cd638d8c9f80d3e93f32f6b0160908043826d
vick-ram <vickramodero6@gmail.com>
2024-04-13 08:36:42 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,15 @@
 import sys
 import db_connector
 
+
 if __name__ == "__main__":
+    """
+    The main function of the script.
+
+    It takes command line arguments for the username, password,
+    and database name,
+    connects to the database, fetches all states, and prints them.
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
