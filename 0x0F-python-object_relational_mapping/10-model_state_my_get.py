2f520fcfffe2cb797fbb92742f29c1ed62be5304
vick-ram <vickramodero6@gmail.com>
2024-04-13 11:54:44 -0700

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Prints the State object with the name passed as argument from the database hbtn_0e_6_usa"""
+"""Prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -11,7 +13,10 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, database))
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, database)
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
 
