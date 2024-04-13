b15f376f8596cacc7b455e206a29c37266241254
vick-ram <vickramodero6@gmail.com>
2024-04-13 08:12:37 -0700

    database connector

---
+++ 0x0F-python-object_relational_mapping/db_connector.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import MySQLdb
+
+def connect_to_db(username, password, database):
+    return MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
