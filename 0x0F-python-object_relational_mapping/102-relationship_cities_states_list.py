4824a437a9690ccc959a7bcce5df22e07de7b32a
vick-ram <vickramodero6@gmail.com>
2024-04-13 09:14:27 -0700

    From city

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -8,12 +8,17 @@ from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
-    """Get MySQL username, password, and database name from command line arguments"""
+    """Get MySQL username, password, and database name from
+    command line arguments
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(username, password, database)
+    )
 
     Base.metadata.create_all(engine)
 
