5af06c742ff671ca6163a918d7ebf577e7c27125
vick-ram <vickramodero6@gmail.com>
2024-04-25 13:56:23 -0700

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Script to get the size of the body of an HTTP response
-url=$1
-response=$(curl -s $url)
-size=$(echo -n "$response" | wc -c)
-echo "$size"
+echo -n "$(curl -s $1)" | wc -c
