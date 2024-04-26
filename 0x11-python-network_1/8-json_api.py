183f9001cc3aab1fb833ff7a36acd17619e54497
vick-ram <vickramodero6@gmail.com>
2024-04-26 12:42:30 -0700

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+import json
+
+if len(sys.argv) < 2:
+    q = ""
+else:
+    q = sys.argv[1]
+
+try:
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    data = response.json()
+    if data:
+        print("[{:d}] {}".format(data['id'], data['name']))
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
+except requests.exceptions.RequestException as e:
+    print("Error:", e)
