4081e7a1b172c101009d447a6c052731a8e6a8a3
vick-ram <vickramodero6@gmail.com>
2024-04-26 13:14:54 -0700

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,24 +5,39 @@ with the letter as a parameter.
 """
 import requests
 import sys
-import json
 
-if len(sys.argv) < 2:
-    q = ""
-else:
-    q = sys.argv[1]
 
-try:
-    response = requests.post(
-        'http://0.0.0.0:5000/search_user',
-        data={'q': q}
-    )
-    data = response.json()
-    if data:
-        print("[{:d}] {}".format(data['id'], data['name']))
+def search_user(letter):
+    """
+    Send a POST request to http://0.0.0.0:5000/search_user
+    with the given letter as a parameter.
+
+    Args:
+        letter (str): The letter to be used as
+        the search parameter.
+    """
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": letter}
+
+    try:
+        response = requests.post(url, data=data)
+        response.raise_for_status()
+
+        if response.json():
+            user = response.json()
+            print(f"[{user['id']}] {user['name']}")
+        else:
+            print("No result")
+    except requests.exceptions.RequestException as e:
+        print("Not a valid JSON")
+    except ValueError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
     else:
-        print("No result")
-except ValueError:
-    print("Not a valid JSON")
-except requests.exceptions.RequestException as e:
-    print("Error:", e)
+        letter = ""
+
+    search_user(letter)
