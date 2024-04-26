97cfc71cbc91e53c3991fcd2ea2e710bff82b0ab
vick-ram <vickramodero6@gmail.com>
2024-04-26 13:09:32 -0700

    Search api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """script that takes in a letter and sends a POST request
-to http://0.0.0.0:5000/search_user with the letter as a parameter.
+to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
 import requests
 import sys
@@ -12,7 +13,10 @@ else:
     q = sys.argv[1]
 
 try:
-    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    response = requests.post(
+        'http://0.0.0.0:5000/search_user',
+        data={'q': q}
+    )
     data = response.json()
     if data:
         print("[{:d}] {}".format(data['id'], data['name']))
